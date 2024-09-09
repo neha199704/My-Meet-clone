8245840ab81ea8997e37e6826bfb44a629e63927
neha199704 <neharawat0408@gmail.com>
2024-09-09 11:52:59 +0530

    Working add info and add people

--- myMeet.js
+++ myMeet.js
@@ -2,12 +2,19 @@ let userGroup = document.getElementById("userGroup");
 let addPeople = document.getElementById("addPeople");
 let crossAside = document.getElementById("crossAside");
 let crossAddPeople = document.getElementById("crossAddPeople");
+let info = document.getElementById("info");
 
 userGroup.addEventListener("click", hideAside);
 
 function hideAside() {
   let aside = document.getElementById("aside");
-  aside.style.display = aside.style.display != "none" ? "none" : "block";
+  console.log(aside.style.right);
+  aside.style.right = aside.style.right != "0px" ? "0px" : "-400px";
+  let imgParentDiv = document.getElementById("imgParentDiv");
+  imgParentDiv.style.width =
+    imgParentDiv.style.width === "calc(100% - 380px)"
+      ? "100%"
+      : "calc(100% - 380px)";
 }
 
 crossAside.addEventListener("click", hideAside);
@@ -21,3 +28,15 @@ function hideAddPeopleDiv() {
 }
 
 crossAddPeople.addEventListener("click", hideAddPeopleDiv);
+
+info.addEventListener("click", () => {
+  let aside2 = document.getElementById("aside2");
+
+  console.log(aside2.style.right);
+  aside2.style.right = aside2.style.right != "0px" ? "0px" : "-400px";
+  let imgParentDiv = document.getElementById("imgParentDiv");
+  imgParentDiv.style.width =
+    imgParentDiv.style.width === "calc(100% - 380px)"
+      ? "100%"
+      : "calc(100% - 380px)";
+});
